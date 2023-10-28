b1253d00da38be69febc0d950a44f54ea463643a
ziaq <sekolah.arief@gmail.com>
2023-10-28 11:55:45 +0700

    feat(tailwind): add tailwind css

--- app/View/pages/index.php
+++ app/View/pages/index.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $model['title'] ?></title>
+    <link rel="stylesheet" href="../../../dist/output.css">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-   
+   <div class="">
+    sf
+   </div>
 </body>
 </html>
