a2eac362783c9f40f74bb4acc3b1f72a153e5f28
ziaq <sekolah.arief@gmail.com>
2023-10-28 13:42:53 +0700

    feat(hero): add hero section

---
+++ app/View/components/hero.php
@@ -0,0 +1,15 @@
+<section class="flex px-10">
+    <div class="flex flex-col justify-center  w-full">
+        <img class="w-[500px] mx-auto h-auto" src="img/hero.svg" alt="not found">
+        <section class="font-bold max-w-lg mx-auto mt-5 text-2xl lg:text-4xl uppercase text-center">
+            <div>Welcome To <span class="hover:p-2 md:p-2 md:bg-black md:text-white transition-all ease-in-out hover:my-3 hover:inline rounded-xl hover:bg-black hover:text-white">ZiaqEdu</span></div>
+            <div class="text-lg lg:text-2xl font-normal mt-5">BE A LONG LIFE LEARNER!</div> <br>
+            <div class="font-thin text-lg">
+                Your swift portal to coding brilliance! Unleash the power of learning with concise and impactful video tutorials. Let's code together!
+            </div>
+        </section>
+        <a class="py-4 px-10 mx-auto my-10 w-max rounded-full min-w-[90px] border border-black ease-in-out hover:bg-black hover:text-white transition-all">
+            Jump To Course
+        </a>
+    </div>
+</section>
--- app/View/pages/index.php
+++ app/View/pages/index.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $model['title'] ?></title>
-    <link rel="stylesheet" href="../../../dist/output.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
+
 <body>
-   <div class="lg:px-20 px-10">
+    <div class="lg:px-20 px-10">
         <?php include __DIR__ . "/../components/navbar.php" ?>
-   </div>
+        <?php include __DIR__ . "/../components/hero.php" ?>
+    </div>
 </body>
+
 </html>
