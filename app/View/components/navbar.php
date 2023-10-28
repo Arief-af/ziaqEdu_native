00228ca1f5ab925520e3e4b0214a808055ab84d8
ziaq <sekolah.arief@gmail.com>
2023-10-28 12:47:59 +0700

    fix(nav): add responsive

--- app/View/components/navbar.php
+++ app/View/components/navbar.php
@@ -4,17 +4,26 @@
     </section>
     <section class="hidden md:block">
         <ul class="flex gap-5 items-center">
-            <li><a href="" class="border-b-2 p-2 border-transparent hover:border-black transition-all duration-300 ease-in-out">Course</a></li>
+            <li>
+                <a href="" class="border-b-2 p-2 border-transparent flex items-center hover:border-black transition-all duration-300 ease-in-out">
+                    <i class='mr-2 text-2xl bx bx-book'></i>Course
+                </a>
+            </li>
 
             <li class="flex gap-5">
                 <button class="p-2 rounded-full min-w-[90px] border border-black ease-in-out hover:bg-black hover:text-white transition-all">
-                    Register
+                    Sign Up
                 </button>
                 <button class="p-2 rounded-full min-w-[90px] border border-black ease-in-out bg-black text-white transition-all">
-                    Login
+                    Sign In
                 </button>
             </li>
-
+        </ul>
+    </section>
+    <section class="md:hidden flex">
+        <ul class="flex text-2xl items-center gap-2">
+            <li class="flex items-center"><i class='bx bx-book'></i></li>
+            <li class="flex items-center"><i class='bx bx-exit'></i><span class="hidden sm:block text-sm ml-2">Sign In</span></li>
         </ul>
     </section>
 </nav>
